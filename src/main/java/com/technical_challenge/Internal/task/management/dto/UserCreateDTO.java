8b7d72f215c492989fec97051bb0ecfb32aab31a
Ana Claudia Santana <anaclaudiasantanadev@gmail.com>
2025-08-28 20:29:16 -0300

    Criar DTO para criação de Usuário UserCreateDTO: Usado para receber dados na criação -> POST /user

---
+++ src/main/java/com/technical_challenge/Internal/task/management/dto/UserCreateDTO.java
@@ -0,0 +1,42 @@
+package com.technical_challenge.Internal.task.management.dto;
+
+import com.technical_challenge.Internal.task.management.models.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+
+public class UserDTO {
+    @NotBlank(message = "O nome é obrigatório.")
+    private String name;
+
+    @Email(message = "Deve ser um email válido.")
+    @NotBlank(message = "O email é obrigatório.")
+    private String email;
+
+    public UserDTO(){
+    }
+
+    public UserDTO(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+}
+
+
