ec19ca35860884631f08266ccf62b5a24ae7e0cb
Ana Claudia Santana <anaclaudiasantanadev@gmail.com>
2025-08-28 14:48:20 -0300

    feat: configuração inicial do projeto Spring Boot com JDK 21 Projeto criado via Spring Initializr Dependencias: Spring Web, JPA, MySQL, DevTools Configurado para Java 21 Estrutura base do Maven

---
+++ src/main/java/com/technical_challenge/Internal/task/management/InternalTaskManagementApplication.java
@@ -0,0 +1,13 @@
+package com.technical_challenge.Internal.task.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternalTaskManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternalTaskManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technical_challenge/Internal/task/management/InternalTaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technical_challenge.Internal.task.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternalTaskManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
