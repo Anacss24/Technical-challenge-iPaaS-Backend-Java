ca26d4f430f5e4d922c07cc4bd2c01275bbd5279
Ana Claudia Santana <anaclaudiasantanadev@gmail.com>
2025-08-30 17:19:05 -0300

    feat: implementar findByStatus e existsByStatus

--- src/main/java/com/technical_challenge/Internal/task/management/repositories/TaskRepository.java
+++ src/main/java/com/technical_challenge/Internal/task/management/repositories/TaskRepository.java
@@ -1,11 +1,18 @@
 package com.technical_challenge.Internal.task.management.repositories;
 
+import com.technical_challenge.Internal.task.management.models.StatusTask;
 import com.technical_challenge.Internal.task.management.models.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface TaskRepository extends JpaRepository<Task, UUID>  {
+public interface TaskRepository extends JpaRepository<Task, UUID> {
 
+    List<Task> findByStatus(StatusTask status);
+
+    boolean existsByStatus(StatusTask status);
 }
+
