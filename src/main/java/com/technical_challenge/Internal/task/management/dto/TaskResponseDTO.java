0406a8141c068116e0a4af0574b88423418e8878
Ana Claudia Santana <anaclaudiasantanadev@gmail.com>
2025-08-30 17:29:26 -0300

    Adicionar dados do usuário no TaskResponseDTO

--- src/main/java/com/technical_challenge/Internal/task/management/dto/TaskResponseDTO.java
+++ src/main/java/com/technical_challenge/Internal/task/management/dto/TaskResponseDTO.java
@@ -3,26 +3,36 @@ package com.technical_challenge.Internal.task.management.dto;
 import com.technical_challenge.Internal.task.management.models.StatusTask;
 import com.technical_challenge.Internal.task.management.models.Task;
 import lombok.Data;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
 public class TaskResponseDTO {
 
-    private UUID id_task;
+    private UUID taskId;
     private String title;
     private String description;
+    @Setter
     private StatusTask status;
     private LocalDateTime creationDate;
     private LocalDateTime completionDate;
+    private UUID userId;
+    private String nomeUser;
+    private String emailUser;
 
 
     public TaskResponseDTO(Task task) {
-        this.id_task = task.getTaskId();
+        this.taskId = task.getTaskId();
         this.title = task.getTitle();
         this.description = task.getDescription();
         this.status = task.getStatus();
         this.creationDate = task.getCreationDate();
         this.completionDate = task.getCompletionDate();
+        this.userId = task.getUser().getUserId();
+        this.nomeUser = task.getUser().getName();
+        this.emailUser = task.getUser().getEmail();
     }
+
 }
