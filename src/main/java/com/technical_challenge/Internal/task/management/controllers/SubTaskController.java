f0c67dfe85ea38463b23e86e5e640ee24cd073fd
Ana Claudia Santana <anaclaudiasantanadev@gmail.com>
2025-09-04 00:15:21 -0300

    feat: Implementar os endpoints GET e PATCH

--- src/main/java/com/technical_challenge/Internal/task/management/controllers/SubTaskController.java
+++ src/main/java/com/technical_challenge/Internal/task/management/controllers/SubTaskController.java
@@ -8,23 +8,41 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.UUID;
+
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
-@RequestMapping("/subTask")
+@RequestMapping("/task")
 public class SubTaskController {
 
     @Autowired
     SubTaskService subTaskService;
 
-    @PostMapping
-    public ResponseEntity<SubTaskResponseDTO> createSubTask(@Validated @RequestBody SubTaskCreateDTO dto){
-        SubTaskResponseDTO savedSubTask = subTaskService.createSubTask(dto);
+    @PostMapping("/{taskId}/subtask")
+    public ResponseEntity<SubTaskResponseDTO> createSubTask(@Validated @RequestBody SubTaskCreateDTO dto, @PathVariable UUID taskId ){
+        SubTaskResponseDTO savedSubTask = subTaskService.createSubTask(dto,taskId);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedSubTask);
     }
+
+    @GetMapping("/{taskId}/subtask")
+    public ResponseEntity<List<SubTaskResponseDTO>> searchTasksById(@Validated @PathVariable UUID taskId){
+        List<SubTaskResponseDTO> tasksById = subTaskService.findSubTasksByTaskId(taskId);
+        return ResponseEntity.ok(tasksById);
+    }
+    @PatchMapping(value = "/subtask/{id}/status")
+    public ResponseEntity<SubTaskResponseDTO> updateStatus(@PathVariable UUID id, @RequestBody SubTaskResponseDTO statusDto) {
+        SubTaskResponseDTO subTaskUpdate = subTaskService.updateSubTaskStatus(id, statusDto.getStatusSub());
+        return ResponseEntity.ok(subTaskUpdate);
+    }
+
 
 }
